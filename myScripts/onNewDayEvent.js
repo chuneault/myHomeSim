417ac186d2de350d23ad105a036a5b2927dce39f
Carl <chuneault@gmail.com>
2017-10-20 07:54:52 -0400

    fix newDay

--- myScripts/onNewDayEvent.js
+++ myScripts/onNewDayEvent.js
@@ -67,9 +67,9 @@ server.on('sunrise', function(){
 
     for (let i=1; i<=3; i++) {
       let sensor = server.vars['AQUALEDCOLOR'];
-      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, 40, 'ffffff');
+      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, '40', 'ffffff');
       sensor = server.vars['AQUALEDBRIGHT'];
-      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, 23, '255');
+      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, '23', '255');
     }
 });
 
@@ -78,9 +78,9 @@ server.on('sunset', function(){
 
     for (let i=1; i<=3; i++) {
       let sensor = server.vars['AQUALEDCOLOR'];
-      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, 40, '472931');
+      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, '40', '073763');
       sensor = server.vars['AQUALEDBRIGHT'];
-      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, 23, '63');
+      sensor.__ownerNode.__ownerDevice.send(sensor.__ownerNode, sensor, '23', '63');
     }
 });
 
