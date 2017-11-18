b19b18a20fbfd77815ce35798397f05d69a8fd55
Carl <chuneault@gmail.com>
2017-11-18 09:47:48 -0500

    fix update vultr

--- myScripts/updateVultrDns.js
+++ myScripts/updateVultrDns.js
@@ -11,12 +11,15 @@ unirest.get('https://api.vultr.com/v1/dns/records')
     .query({domain: "huneault.ca"})
     .end(function (response) {
       var dnsHome = _.find(response.body, {name: 'home'});
-      unirest.post('https://api.vultr.com/v1/dns/update_record')
+      if ((dnsHome) && (dnsHome.RECORDID)) {
+        unirest.post('https://api.vultr.com/v1/dns/update_record')
           .headers({'API-Key': '7JPMNZFXC3LFSZ7TZWKSAU7FYOVP2PZ2XZUA'})
           .send({RECORDID: dnsHome.RECORDID, data: sensor.lastValue, domain: 'huneault.ca' })
           .end(function (response) {
             //console.log(response.body);
           });
+      } else
+        console.log('Vultr DNS Update Error', dnsHome);
     });
 
 }
