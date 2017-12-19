8c6e6a195dbce0453f5c90a2980cd76bea239f1d
chuneault <chuneault@gmail.com>
2017-12-19 09:40:01 -0500

    fix update sensors

--- test.js
+++ test.js
@@ -1,28 +1,5 @@
 
-const _ = require('lodash');
 
+var PouchDB = require('pouchdb');
 
-var nodes = [];
-
-
-nodes.push({deviceId: 2});
-nodes.push({deviceId: 1, version: 10, vendor:{id:2, name: "bonjour", version: 10}});
-
-
-//var node = _.find(nodes, {deviceId: 1, vendor: {id:2}});
-var node = _.find(nodes, {deviceId: 1});
-
-console.log('found node', node);
-
-var updatenode = {vendor: {name: 'unknown'}};
-
-/*if (updatenode.vendor) {
-    if (node.vendor)
-      _.extend(node.vendor, updatenode.vendor);
-    else
-      node.vendor = updatenode.vendor;
-    delete (updatenode.vendor);
-}*/
-_.extend(node, updatenode);
-
-console.log('updated node', node);
+var TempPouchDB = PouchDB.defaults({prefix: '~/pouchdb-server/myHomeSim'});
