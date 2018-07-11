75a79823bcd87f2d2c0721907aba257b7d65196a
chuneault <chuneault@gmail.com>
2018-07-11 13:28:08 -0400

    new mongodb database

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   secret: 'ilovescotchyscotch',
-  db: {master: 'http://192.168.0.150:5984/myHomeSim', sensorsValues: 'http://192.168.0.150:5984/sensorsValues'},
+  db: {url: 'mongodb://192.168.0.168:27017', dbName: 'myhomesim'},
   cryptoOptions: {iterations: 256, keylen: 512},
   logger: {
     core: './logs/cor.log',
