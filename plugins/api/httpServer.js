64006f87f63fffa62f2fb37c1b5472b7172c7fc0
chuneault@gmail.com <Hintendo40>
2018-09-04 11:22:23 -0400

    upload picture sensor

--- plugins/api/httpServer.js
+++ plugins/api/httpServer.js
@@ -512,7 +512,8 @@ class httpServer extends plugins {
           if (!req.files)
               return res.status(400).send('No files were uploaded.');
 
-          // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+          console.log(req.files);
+          /* The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
           let sampleFile = req.files.sampleFile;
 
           // Use the mv() method to place the file somewhere on your server
@@ -523,6 +524,8 @@ class httpServer extends plugins {
 
               res.send('File uploaded!');
           });
+
+          */
       });
 
 
